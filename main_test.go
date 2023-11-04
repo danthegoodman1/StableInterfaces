178fce3c4756b13a914a5ce1ce8b853ad716cab2
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2023-11-04 10:38:52 -0400

    better testing

--- main_test.go
+++ main_test.go
@@ -36,7 +36,8 @@ func (ti *TestInterface) OnRequest(ctx context.Context, payload any) (any, error
 }
 
 func TestStableInterfaceSimple(t *testing.T) {
-	im, err := NewInterfaceManager("host-0", "host-{0..1}", 1024, func(internalID string) StableInterface {
+	host := "host-0"
+	im, err := NewInterfaceManager(host, "host-{0..1}", 1024, func(internalID string) StableInterface {
 		return &TestInterface{
 			internalID: internalID,
 		}
@@ -44,8 +45,19 @@ func TestStableInterfaceSimple(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	id1 := "1"
 
+	// Check hosts
+	instanceHost, err := im.GetHostForID(id1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if instanceHost != host {
+		t.Fatalf("got mismatched hosts %s and %s", host, instanceHost)
+	}
+
+	// Check id
 	internalID1, err := im.GetInternalID(id1)
 	if err != nil {
 		t.Fatal(err)
@@ -59,6 +71,7 @@ func TestStableInterfaceSimple(t *testing.T) {
 		t.Fatalf("did not get matching internal ID, got: %+v", s)
 	}
 
+	// Check error handling
 	res, err = im.Request(context.Background(), id1, TestInterfaceReturnError)
 	if err != nil {
 		if !errors.Is(err, TestError) || !errors.Is(err, StableInterfaceHandlerErr) {
@@ -67,5 +80,11 @@ func TestStableInterfaceSimple(t *testing.T) {
 	}
 	if err == nil {
 		t.Fatal("was expecting error")
+	}
+
+	// Test wrong host
+	_, err = im.Request(context.Background(), "3", nil)
+	if !errors.Is(err, ErrHostDoesNotOwnShard) {
+		t.Fatal("did not get host does not own shard")
 	}
 }
